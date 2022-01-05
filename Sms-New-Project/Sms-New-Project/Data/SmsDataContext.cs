b7beac9f419891cee3e03d354656d0ba9c0f059a
Emanaws <redwanomer44@gmail.com>
2022-01-05 01:06:05 -0600

    MVC Project

---
+++ Sms-New-Project/Sms-New-Project/Data/SmsDataContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sms_New_Project.Data
+{
+    public class SmsDataContext
+    {
+    }
+}
---
+++ Sms-New-Project/Sms-New-Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sms_New_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
